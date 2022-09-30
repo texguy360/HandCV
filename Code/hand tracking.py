7a5b25c0d072b657abd6b58356fa26aee79224b9
Ryan Li <rl31@illinois.edu>
2022-09-29 22:08:36 -0500

    initial code

--- Code/hand tracking.py
+++ Code/hand tracking.py
@@ -1,6 +1,5 @@
 import cv2
 import mediapipe as mp
-import time
 
 cap = cv2.VideoCapture(0)
 
@@ -19,8 +18,6 @@ while True:
             for id, lm in enumerate(handLms.landmark):
                 h, w, c = img.shape
                 cx, cy = int(lm.x * w), int(lm.y*h)
-                print(cx)
-                # cv2.circle(img, (cx, cy), 3, (255, 0, 255), cv2.FILLED)
 
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
 
