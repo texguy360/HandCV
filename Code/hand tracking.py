d30b1dbe278cdf91ec18eb20155f09e3952f166b
Ryan Li <rl31@illinois.edu>
2022-10-06 16:39:55 -0500

    corrections for initial code

--- Code/hand tracking.py
+++ Code/hand tracking.py
@@ -11,18 +11,12 @@ while True:
     success, img = cap.read()
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = hands.process(imgRGB)
-    # print(results.multi_hand_landmarks)
 
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
             for id, lm in enumerate(handLms.landmark):
                 h, w, c = img.shape
                 cx, cy = int(lm.x * w), int(lm.y*h)
-<<<<<<< HEAD
-=======
-                print(cx)
->>>>>>> 9995d834180b6df54ed318554746a7b0fd134d18
-
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
 
     cv2.imshow("Image", img)
