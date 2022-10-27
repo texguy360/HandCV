133a95f09a959d9198d118cdc96dd507ff8e9fb8
Ryan Li <rl31@illinois.edu>
2022-10-27 17:29:57 -0500

    new file for testing

--- Code/finger bend.py
+++ Code/finger bend.py
@@ -39,14 +39,6 @@ class handDetector():
                 lmlist.append([id, cx, cy])
         return lmlist
 
-    #def indexFinger(self, lmlist):
-        #while True:
-            #index_base05 = self.sqrt(self.pow(lmlist[5][1] - lmlist[0][1], 2) + self.pow(lmlist[5][2] - lmlist[0][2], 2))
-            #index_tip85 = self.sqrt(self.pow(lmlist[8][1] - lmlist[5][1], 2) + self.pow(lmlist[8][2] - lmlist[5][2], 2))
-            #index_bend = int(index_tip85)/int(index_base05)
-        #return index_bend
-
-
 def main():
     cap = cv2.VideoCapture(0)
     detector = handDetector()
@@ -62,13 +54,36 @@ def main():
             index_tip8_0 = math.sqrt(math.pow(lmlist[8][1] - lmlist[0][1], 2) + math.pow(lmlist[8][2] - lmlist[0][2], 2))
             index_bend = index_tip8_0/index_base0_5
 
-            if index_bend < 1.7:
-                index = 1
-            elif index_bend < 1:
-                index = 2
+            middle_base0_9 = math.sqrt(
+                math.pow(lmlist[9][1] - lmlist[0][1], 2) + math.pow(lmlist[9][2] - lmlist[0][2], 2))
+            middle_tip12_0 = math.sqrt(math.pow(lmlist[12][1] - lmlist[0][1], 2) + math.pow(lmlist[12][2] - lmlist[0][2], 2))
+            middle_bend = middle_tip12_0/middle_base0_9
 
+            ring_base0_13 = math.sqrt(
+                math.pow(lmlist[13][1] - lmlist[0][1], 2) + math.pow(lmlist[13][2] - lmlist[0][2], 2))
+            ring_tip16_0 = math.sqrt(
+                math.pow(lmlist[16][1] - lmlist[0][1], 2) + math.pow(lmlist[16][2] - lmlist[0][2], 2))
+            ring_bend = ring_tip16_0/ring_base0_13
 
+            small_base0_17 = math.sqrt(
+                math.pow(lmlist[17][1] - lmlist[0][1], 2) + math.pow(lmlist[17][2] - lmlist[0][2], 2))
+            small_tip20_0 = math.sqrt(
+                math.pow(lmlist[20][1] - lmlist[0][1], 2) + math.pow(lmlist[20][2] - lmlist[0][2], 2))
+            small_bend = small_tip20_0/small_base0_17
 
+            thumb_base1_0 = math.sqrt(
+                math.pow(lmlist[2][1] - lmlist[0][1], 2) + math.pow(lmlist[2][2] - lmlist[0][2], 2))
+            thumb_mid4_17 = math.sqrt(
+                math.pow(lmlist[4][1] - lmlist[17][1], 2) + math.pow(lmlist[4][2] - lmlist[17][2], 2))
+            thumb_rotate = thumb_mid4_17/thumb_base1_0
+
+            thumb_tip4_2 = math.sqrt(
+                math.pow(lmlist[4][1] - lmlist[2][1], 2) + math.pow(lmlist[4][2] - lmlist[2][2], 2))
+            thumb_base3_2 = math.sqrt(
+                math.pow(lmlist[3][1] - lmlist[2][1], 2) + math.pow(lmlist[3][2] - lmlist[2][2], 2))
+            thumb_bend = thumb_tip4_2/thumb_base3_2 #1.67
+
+            print(index_bend)
 
         cv2.imshow("Image", img)
         cv2.waitKey(1)
